1902a6c21ea70613ed42fdb6cd06c3c587a6e544
Yicheng Jin <bobjin42@gmail.com>
2018-08-07 15:45:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+    def name(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed(name)
+    @breed = name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_name)
+    @breed = new_name
+  end
+end
