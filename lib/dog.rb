052062ba550f91642dc0f1dff55392b06d352532
mguevara717 <marissa.guevara717@gmail.com>
2018-08-07 12:58:41 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+  def name=(dogs_name) #setter
+    @name = dogs_name
+  end
+
+  def name #getter
+    @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = "Beagle"
+  end
+
+  def breed
+    @breed
+  end
+end
